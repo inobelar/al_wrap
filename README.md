021044e0bb7cf106b91730cd75c7c4a0662b150a
inobelar <inobelar@gmail.com>
2023-03-30 16:22:24 +0300

    Added (experimental) cmake include file

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 ## Usage
 
 - `qmake`:
-    - Add to your `*.pro` file next lines:
+    - Add into your `*.pro` file the next lines:
 
         ```qmake
         include(path/to/al_wrap.pri)
@@ -17,4 +17,23 @@
         CONFIG(debug, debug|release) {
             DEFINES += ALWRAP_CHECK_FUNCS
         }
+        ```
+- `cmake`:
+    - Add into `CMakeLists.txt` file the next lines:
+
+        ```cmake
+        # Add 'al_wrap' into modules search path - to find this file (al_wrap.cmake)
+        list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/path/to/al_wrap/")
+        include(al_wrap)
+
+        add_executable( <YOUR_EXECUTABLE>
+            # ...
+            ${ALWRAP_HEADERS} ${ALWRAP_SOURCES}
+        )
+
+        # Add 'al_wrap' includes search path
+        target_include_directories( <YOUR_EXECUTABLE> ${ALWRAP_INCLUDE_DIRECTORIES} )
+
+        # Add 'al_wrap' dependency libraries (OpenAL library)
+        target_link_libraries( <YOUR_EXECUTABLE> ${ALWRAP_LIBRARIES} )
         ```
---
+++ al_wrap.cmake
@@ -0,0 +1,54 @@
+#[[
+    # Example of usage
+
+    Add into your CMakeLists.txt the next lines:
+
+    # --------------------------------------------------------------------------
+
+    # Add 'al_wrap' into modules search path - to find this file (al_wrap.cmake)
+    list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/path/to/al_wrap/")
+    include(al_wrap)
+
+    add_executable( <YOUR_EXECUTABLE>
+        # ...
+        ${ALWRAP_HEADERS} ${ALWRAP_SOURCES}
+    )
+
+    # Add 'al_wrap' includes search path
+    target_include_directories( <YOUR_EXECUTABLE> ${ALWRAP_INCLUDE_DIRECTORIES} )
+
+	# Add 'al_wrap' dependency libraries (OpenAL library)
+	target_link_libraries( <YOUR_EXECUTABLE> ${ALWRAP_LIBRARIES} )
+
+    # --------------------------------------------------------------------------
+]]
+
+set(ALWRAP_INCLUDE_DIRECTORIES
+	${CMAKE_CURRENT_LIST_DIR}/include/
+)
+
+set(ALWRAP_HEADERS
+    ${CMAKE_CURRENT_LIST_DIR}/include/al_wrap/al_simple_init.hpp
+    ${CMAKE_CURRENT_LIST_DIR}/include/al_wrap/al_error_checking.hpp
+
+    ${CMAKE_CURRENT_LIST_DIR}/include/al_wrap/al_Listener.hpp
+
+    ${CMAKE_CURRENT_LIST_DIR}/include/al_wrap/objects/al_Object.hpp
+    ${CMAKE_CURRENT_LIST_DIR}/include/al_wrap/objects/al_Buffer.hpp
+    ${CMAKE_CURRENT_LIST_DIR}/include/al_wrap/objects/al_Source.hpp
+)
+
+set(ALWRAP_SOURCES
+    $$PWD/sources/al_simple_init.cpp
+    $$PWD/sources/al_error_checking.cpp
+
+    $$PWD/sources/al_Listener.cpp
+
+    $$PWD/sources/objects/al_Object.cpp
+    $$PWD/sources/objects/al_Buffer.cpp
+    $$PWD/sources/objects/al_Source.cpp
+)
+
+set(ALWRAP_LIBRARIES
+	openal
+)
